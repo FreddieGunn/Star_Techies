df856503c2d9854d3b4979a230350a30ca239379
Freddie <freddiegunn1@gmail.com>
2023-09-22 15:48:22 +0100

    Final updates to change config over to AWS IAM

--- api/__init__.py
+++ api/__init__.py
@@ -4,12 +4,8 @@ from boto3 import client
 
 api = Flask(__name__)
 api.config.from_object(Config)
-s3_client = client('s3', aws_access_key_id=api.config['AWS_ACCESS_KEY_ID'],
-                   aws_secret_access_key=api.config['AWS_SECRET_ACCESS_KEY'],
-                   region_name=api.config['AWS_REGION'])
-dynamodb_client = client('dynamodb', aws_access_key_id=api.config['AWS_ACCESS_KEY_ID'],
-                         aws_secret_access_key=api.config['AWS_SECRET_ACCESS_KEY'],
-                         region_name=api.config['AWS_REGION'])
+s3_client = client('s3')
+dynamodb_client = client('dynamodb')
 
 
 # Flask requires these to be imported at the bottom
--- config.py
+++ config.py
@@ -5,7 +5,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "SET_ALTERNATIVE_SECRET_KEY"
     SECRET_REFRESH_KEY = os.environ.get("SECRET_REFRESH_KEY") or "SET_ALTERNATIVE_SECRET_REFRESH_KEY"
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_BUCKET_NAME = os.environ.get("AWS_BUCKET_NAME")
-    AWS_REGION = os.environ.get("AWS_REGION")
