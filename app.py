f3b374f68ed7bee2eabc78f2b0b47a55bb725caa
EC2 Default User <ec2-user@ip-172-31-88-42.ec2.internal>
2023-09-22 18:00:48 +0000

    Added staff view for residents

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
+import sys
+sys.path.insert(0, '/home/ec2-user/environment/Star_Techies')
+
 from flask import Flask, jsonify
+from resident.resident_home import resident_api
+from staff.staff_view_endpoints import staff_api
 import boto3
 
 
 app = Flask(__name__)
 
+app.register_blueprint(resident_api)
+app.register_blueprint(staff_api)
 
 @app.route("/")
 def hello():
     return "Hello World!"
     
-@app.route('/get-items/<ID>')
-def get_items(ID):
-    dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
-    return dynamo_client.get_item(
-        TableName='Residents', Key={"ID": {"S":ID}})
-    
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
--- resident/resident_home.py
+++ resident/resident_home.py
@@ -1,6 +1,26 @@
-import resident_dynamodb_connection
+from flask import Blueprint, jsonify
+from utils import simplify_json_response
 
-def get_beneficiary_details_by_ID(ID):
+import boto3
+
+resident_api = Blueprint('resident_api', __name__)
+
+
+dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
+
+@resident_api.route('/get-items/<ID>')
+def get_items(ID):
+    dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
+    response = dynamo_client.get_item(
+        TableName='Residents', Key={"ID": {"S":ID}})
+    low_level_data = response["Item"]
+    return simplify_json_response(low_level_data)
+    
+    
+
+    
+    
+        
     
     
     
