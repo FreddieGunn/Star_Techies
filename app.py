6689e1d3752569c788a9ab41b3058978f120b089
EC2 Default User <ec2-user@ip-172-31-88-42.ec2.internal>
2023-09-22 13:23:44 +0000

    added endpoint for returning resident details

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+import boto3
 
 
 app = Flask(__name__)
@@ -7,6 +8,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+    
+@app.route('/get-items/<ID>')
+def get_items(ID):
+    dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
+    return dynamo_client.get_item(
+        TableName='Residents', Key={"ID": {"S":ID}})
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
---
+++ resident/resident_home.py
@@ -0,0 +1,6 @@
+import resident_dynamodb_connection
+
+def get_beneficiary_details_by_ID(ID):
+    
+    
+    
